14d61e3b91c58012d9508ee91311e9e8ef9ecfa9
lavakumar <lavakumar.munja@gmail.com>
2021-05-29 20:07:41 +0530

    Revert "adding agin"
    This reverts commit 9fa15ac164cb95602ba16225f757a3243e96a25a.

--- 07-Conditionals/src/com/in28minutes/ifstatement/examples/MenuRunner.java
+++ 07-Conditionals/src/com/in28minutes/ifstatement/examples/MenuRunner.java
@@ -6,7 +6,6 @@ public class MenuRunner {
 	public static void main(String[] args) {
 		// Type obj = new Type(argument);
 		//Added something
-		//adding again
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter Number1: ");
 		int number1 = scanner.nextInt();
