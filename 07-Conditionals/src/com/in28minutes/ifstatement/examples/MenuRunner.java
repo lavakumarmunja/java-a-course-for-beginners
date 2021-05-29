d1c62cf66fdc6726253e18ab445e4cb75d23ba4a
lavakumar <lavakumar.munja@gmail.com>
2021-05-29 20:06:24 +0530

    adding now

--- 07-Conditionals/src/com/in28minutes/ifstatement/examples/MenuRunner.java
+++ 07-Conditionals/src/com/in28minutes/ifstatement/examples/MenuRunner.java
@@ -6,6 +6,7 @@ public class MenuRunner {
 	public static void main(String[] args) {
 		// Type obj = new Type(argument);
 		//Added something
+		//Adding now
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter Number1: ");
 		int number1 = scanner.nextInt();
