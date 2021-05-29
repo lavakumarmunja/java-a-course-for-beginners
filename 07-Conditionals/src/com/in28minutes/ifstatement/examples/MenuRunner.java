9fa15ac164cb95602ba16225f757a3243e96a25a
lavakumar <lavakumar.munja@gmail.com>
2021-05-29 20:07:25 +0530

    adding agin

--- 07-Conditionals/src/com/in28minutes/ifstatement/examples/MenuRunner.java
+++ 07-Conditionals/src/com/in28minutes/ifstatement/examples/MenuRunner.java
@@ -6,6 +6,7 @@ public class MenuRunner {
 	public static void main(String[] args) {
 		// Type obj = new Type(argument);
 		//Added something
+		//adding again
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter Number1: ");
 		int number1 = scanner.nextInt();
